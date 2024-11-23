7f76fb6eb324ecd7361363107c455b192c5af779
ssw0420 <tmddn5645@gmail.com>
2024-11-24 01:04:05 +0900

    summonerId Update
    summonerId Update

---
+++ ChallengerLeagues_SummonerId.py
@@ -0,0 +1,28 @@
+import requests
+import json
+from urllib import parse
+from dotenv import load_dotenv
+import os
+
+json_file_names = ['challengerleagues.json', 'grandmasterleagues.json', 'masterleagues.json']
+
+
+# with open('challengerleagues.json', 'r') as file:
+#      data_json = file.read()
+
+# data = json.loads(data_json)
+# challengerleagues_summoner_ids = [entry['summonerId'] for entry in data['entries']]
+
+summoner_ids = []
+
+for file_name in json_file_names:
+    with open(file_name, 'r') as file:
+        data = json.load(file)
+        summoner_ids.extend([entry['summonerId'] for entry in data['entries']])
+
+summoner_ids = list(set(summoner_ids))
+
+with open('all_summoner_ids.json', 'w') as output_file:
+    json.dump(summoner_ids, output_file, indent=4)
+
+print("챌린저, 그랜드마스터, 마스터 json 소환사 아이디 병합")
